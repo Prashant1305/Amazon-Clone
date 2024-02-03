53eba61fac6750991f7dce1ee76ca0fa6cdea69c
Parasmanithakur <paras.parasthakur.thakur@gmail.com>
2024-02-03 13:57:17 +0530

    changes

--- client/src/components/home/Banner.js
+++ client/src/components/home/Banner.js
@@ -1,41 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import Carousel from 'react-material-ui-carousel';
+import React, { useEffect, useState } from "react";
+import Carousel from "react-material-ui-carousel";
 import "./Banner.css";
 import { baseUrl } from "../../Constant";
 
 function Banner() {
-    const [data, setData] = useState();
-    const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
-    const fetchBannerData = async () => {
-        try {
-            setIsLoading(true);
-            const response = await fetch(`${baseUrl}/api/banner`);
-            if (response.status == 200) {
-                const response_data = await response.json();
-                setData(response_data.msg);
-                setIsLoading(false);
-            }
-        } catch (error) {
-            console.log(error);
-        }
+  const fetchBannerData = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`${baseUrl}/api/banner`);
+      if (response.status === 200) {
+        const response_data = await response.json();
+        setData(response_data.msg);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(() => {
-        fetchBannerData();
-    }, []);
-    if (isLoading) {
-        return <h1>Loading...</h1>
-    }
-    return (
-        <Carousel className='carasousel' animation='slide' indicators={false} navButtonAlwaysVisible={true}  >
-            {
-                data.map((item) => {
-                    return (<img src={item.url} key={item._id} alt="Failed to load" className='banner_img' />)
-                })
-            }
-        </Carousel>
-    )
+  useEffect(() => {
+    fetchBannerData();
+  }, []);
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  return (
+    <Carousel
+      className="carasousel"
+      animation="slide"
+      indicators={false}
+      navButtonAlwaysVisible={true}
+    >
+      {data.map((item) => {
+        return (
+          <img
+            src={item.url}
+            key={item._id}
+            alt="Failed to load"
+            className="banner_img"
+          />
+        );
+      })}
+    </Carousel>
+  );
 }
 
-export default Banner
+export default Banner;
