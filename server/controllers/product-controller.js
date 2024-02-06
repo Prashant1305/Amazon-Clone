f783164cb824290def6958b68b3273a28056ed02
Parasmanithakur <paras.parasthakur.thakur@gmail.com>
2024-02-06 23:32:50 +0530

    getProduct by category and BUG fix reacr warning s

--- server/controllers/product-controller.js
+++ server/controllers/product-controller.js
@@ -2,25 +2,31 @@ const Product = require("../models/product-model");
 
 const topTendiscountedProducts = async (req, res, next) => {
   try {
-    const response = await Product.find().sort({ discount_percentage: -1 }).limit(10).select({ _id: 1, url: 1, name: 1, discount_percentage: 1, category: 1 });
+    const response = await Product.find()
+      .sort({ discount_percentage: -1 })
+      .limit(10)
+      .select({ _id: 1, url: 1, name: 1, discount_percentage: 1, category: 1 });
     console.log("topTendiscountedProducts data sent");
     res.status(200).json({ msg: response });
   } catch (error) {
     console.log(error);
     next(error);
   }
-}
+};
 
 const topTwentyRatedProducts = async (req, res, next) => {
   try {
-    const response = await Product.find({}).sort({ rating: -1 }).limit(10).select({ _id: 1, url: 1, name: 1, discount_percentage: 1, category: 1 });
+    const response = await Product.find({})
+      .sort({ rating: -1 })
+      .limit(10)
+      .select({ _id: 1, url: 1, name: 1, discount_percentage: 1, category: 1 });
     console.log("topTwentyRatedProducts data sent");
     res.status(200).json({ msg: response });
   } catch (error) {
     console.log(error);
     next(error);
   }
-}
+};
 
 const getSingleProductDetails = async (req, res, next) => {
   try {
@@ -34,6 +40,26 @@ const getSingleProductDetails = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-}
+};
 
-module.exports = { topTendiscountedProducts, topTwentyRatedProducts, getSingleProductDetails };
+const getProductsByCategory = async (req, res, next) => {
+  try {
+    const category = req.params.category;
+    console.log(category);
+    const response = await Product.find({ category: { $regex: category } });
+    if (!response) {
+      res.status(202).json({ msg: "category not found " });
+    }
+    res.status(200).json({ msg: response });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+module.exports = {
+  topTendiscountedProducts,
+  topTwentyRatedProducts,
+  getSingleProductDetails,
+  getProductsByCategory,
+};
