ce2db24e3cf943a86819f6c5596feec3fcd8e4da
prashantSingh <prashantsing13052000@gmail.com>
2024-02-17 23:50:49 +0530

    changed css of productItem.js and item.js and changed little bit of projects structure

--- client/src/components/adminComponents/AdminLayout.js
+++ client/src/components/adminComponents/AdminLayout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import AdminNavbar from "../AdminNavbar";
+import AdminNavbar from "./AdminNavbar";
 
 const AdminLayout = () => {
   return (
